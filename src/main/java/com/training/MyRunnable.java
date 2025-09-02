7e25ca2cfc6f01e3edb22c396a0a52a2513f8dc5
Balaji-V-S <vsbrobot@gmail.com>
2025-09-02 16:42:18 +0530

    check upstream

--- src/main/java/com/training/MyRunnable.java
+++ src/main/java/com/training/MyRunnable.java
@@ -4,6 +4,7 @@ public class MyRunnable implements Runnable{
     @Override
     public void run() {
         System.out.println("My Runnable class - thread name :" + Thread.currentThread().getName());
-        System.out.println("My Runnable class - state : " + Thread.currentThread().getState());
+        System.out.println("Hello I'm under water : )");
+        System.out.println("My Runnable class - state : " + Thread.currentThread().getName());
     }
 }
